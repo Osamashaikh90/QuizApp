49358daebede57272823f4b88f54d057dfb7c8f8
Osama <shaikhosama499@gmail.com>
2023-12-23 15:40:46 +0530

    build(app): basic Setup for the project done.

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    screens: {
+      xxsm: "350px",
+      xsm: "425px",
+      mobile: "425px",
+      sm: "640px",
+      tablet: "640px",
+      md: "768px",
+      lg: "1024px",
+      laptop: "1024px",
+      xl: "1440px",
+      desktop: "1280px",
+      "2xl": "1536px",
+    },
+    extend: {
+      fontFamily: {
+        Cursive: ["Caveat", "cursive"],
+      },
+    },
+  },
+  plugins: [],
+};
