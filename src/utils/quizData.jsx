4a0efd441bd14425d56fceef056071f54fdb8ff2
Osama <shaikhosama499@gmail.com>
2023-12-23 15:44:32 +0530

    chore(app): quiz data added

---
+++ src/utils/quizData.jsx
@@ -0,0 +1,35 @@
+const data = [
+  {
+    question: "What is the purpose of the <body> tag in HTML?",
+    options: [
+      "It defines the document's head section.",
+      "It contains all the content such as text, images, and links.",
+      "It is used to define the main content of an HTML document.",
+      "It specifies the body of the email content in HTML.",
+    ],
+    answer: 2,
+  },
+  {
+    question: "What does CSS stand for?",
+    options: [
+      "Colorful Style Sheets",
+      "Computer Style Sheets",
+      "Cascading Style Sheets",
+      "Creative Style Sheets",
+    ],
+    answer: 3,
+  },
+  {
+    question:
+      "Which syntax is correct to output 'Hello World' in an alert box?",
+    options: [
+      "alert('Hello World');",
+      "alertBox('Hello World');",
+      "msg('Hello World');",
+
+      "msgBox('Hello World');",
+    ],
+    answer: 1,
+  },
+];
+export default data;
