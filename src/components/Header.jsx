02b66859ae5491f2c98258a3b329032f48e80e06
Osama <shaikhosama499@gmail.com>
2023-12-23 15:45:18 +0530

    feat(app): header created

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+// import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <div style={{ backgroundColor: "#1a1a1a" }} className="p-2 ">
+        <h1 className="font-Cursive text-3xl inline-block hover:transform hover:scale-100 cursor-pointer text-red-500 font-medium xxsm:flex xxsm:items-center xxsm:justify-center md:justify-start md:mx-32 ">
+          QuizMania
+        </h1>
+      </div>
+    </>
+  );
+};
+
+export default Header;
